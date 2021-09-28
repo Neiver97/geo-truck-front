cbd6758b09801fcf7b83b255a9ca314c4687edeb
juan david morales muñoz <5810910-juangenoma@users.noreply.gitlab.com>
2021-09-28 00:00:44 -0500

    tabla

--- src/pages/TableList.vue
+++ src/pages/TableList.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="row">
       <div class="col-12">
-        <card :title="table1.title" :subTitle="table1.subTitle">
+        <card >
           <div slot="raw-content" class="table-responsive">
-            <paper-table type="hover" :data="table1.data" :columns="table1.columns">
+            <b-table :items="this.tableData"  caption-top>
 
-            </paper-table>
+            </b-table>
           </div>
         </card>
       </div>
@@ -14,39 +14,36 @@
 <script>
 import { PaperTable } from "@/components";
 import axios from 'axios';
-const tableColumns = ["Id", "Title", "Description", "Start Date", "Finish Date"];
-const tableData =[];
 
 export default {
   components: {
     PaperTable
   },
-  mounted(){
+  created(){
     this.getElements();
   },
   data() {
     return {
       tableData:[],
-      table1: {
-        title: "Activity table",
-        subTitle: "Information about the activitys",
-        columns: [...tableColumns],
-        data: [...tableData]
-      },
-      table2: {
-        title: "Table on Plain Background",
-        subTitle: "Here is a subtitle for this table",
-        columns: [...tableColumns],
-        data: [...tableData]
-      }
+      data:[]
     };
   },
   methods: {
-    async getElements(){
-      await axios.get('http://localhost:8089/actividades/')
+     getElements(){
+       axios.get('http://localhost:8089/actividades/')
       .then(response => {
-        this.tableData=response.data;
-        console.log(tableData);
+        this.data=response.data;
+        this.data.forEach(data => {
+          datos:
+          this.tableData.push({
+            id: data.id,
+            Title: data.titulo,
+            descripcion: data.descripcion,
+            fechaInicio:data.fechaInicio,
+            fechaFin: data.fechaFin
+          })
+          })
+        console.log(this.tableData);
       })
       .catch(e => {
           console.log(e);
